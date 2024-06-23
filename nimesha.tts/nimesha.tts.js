3f77414be52c8eac75dd6300ab854aa4b891606b
Sathsara Modz <145262720+sathsara-technic@users.noreply.github.com>
2024-06-23 18:08:31 +0530

    Create nimesha.tts.js

---
+++ nimesha.tts/nimesha.tts.js
@@ -0,0 +1,16 @@
+var tabCmds = [];
+let cm = [];
+function nimesha.tts(obj, fonctions) {
+    let infoComs = obj;
+    if (!obj.categorie) {
+        infoComs.categorie = "General";
+    }
+    if (!obj.reaction) {
+        infoComs.reaction = "🌟";
+    }
+    infoComs.fonction = fonctions;
+    cm.push(infoComs);
+    // console.log('chargement...')
+    return infoComs;
+}
+module.exports = { nimesha.tts, Module: nimesha.tts, cm };
